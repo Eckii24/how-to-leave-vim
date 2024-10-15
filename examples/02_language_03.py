870be4823c7d1f619925beffb7ff6a9fd3ab6998
Matthias Eck <matthias.eck@zeiss.com>
2024-10-15 08:26:23 +0200

    feat: add example for verbs

---
+++ examples/02_language_03.py
@@ -0,0 +1,37 @@
+# Exended comand form: <multiplier><verb><motion>
+
+# Verb commands:
+# - d        --> delete
+# - c        --> change
+# - y        --> yank / copy
+
+from flask import Flask
+
+app = Flask( name )
+
+@app. route ( " /change_a_light_bulb" )
+def change_a_light_bulb():
+    return {
+        "question": "How many programmers does it take to change a light bulb?"
+        "answer": "Five developers - hopefully they will change it"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#"answer": "None. That's a hardware problem."
+
+
+# 02_language_04.py
